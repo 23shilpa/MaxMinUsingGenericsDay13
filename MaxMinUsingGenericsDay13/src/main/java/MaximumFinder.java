b02d0878db8ad5ef41a8987f710974996aee56d7
shilpa <shilpabhonde000@gmail.com>
2023-09-24 15:57:45 +0530

    UC2 Complete.

--- MaxMinUsingGenericsDay13/src/main/java/MaximumFinder.java
+++ MaxMinUsingGenericsDay13/src/main/java/MaximumFinder.java
@@ -1,3 +1,4 @@
+
 public class MaximumFinder {
 
     public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
@@ -15,8 +16,16 @@ public class MaximumFinder {
     }
 
     public static void main(String[] args) {
-        // Test Case: Find maximum among three floats
-        Float maxFloat = findMax(7.5f, 4.3f, 9.8f);
-        System.out.println("Maximum float is " + maxFloat); // Expected output: 9.8
+        // Test Case 1: Find maximum among three strings
+        String maxString1 = findMax("Apple", "Peach", "Banana");
+        System.out.println("Test Case 1: Maximum string is " + maxString1); // Expected output: "Peach"
+
+        // Test Case 2: Find maximum among three strings (different order)
+        String maxString2 = findMax("Peach", "Apple", "Banana");
+        System.out.println("Test Case 2: Maximum string is " + maxString2); // Expected output: "Peach"
+
+        // Test Case 3: Find maximum among three strings (different order)
+        String maxString3 = findMax("Banana", "Peach", "Apple");
+        System.out.println("Test Case 3: Maximum string is " + maxString3); // Expected output: "Peach"
     }
 }
